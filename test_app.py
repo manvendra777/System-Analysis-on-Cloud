66fa9d3c3e6e7b38dafa8764a0ed87bb3829a885
manvendra777 <manvendrachavan@gmail.com>
2024-11-20 00:52:45 -0800

    updated tests

--- test_app.py
+++ test_app.py
@@ -1,10 +1,14 @@
 import pytest
-from app import app
+from app import app, get_token
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+def auth_token(client):
+    response = client.get('/token')
+    return response.json['token']
+
 def test_authorization(client):
     response = client.get('/cpu', headers={'Authorization': 'dummy-header'})
     assert response.status_code == 401
@@ -12,19 +16,19 @@ def test_authorization(client):
     assert response.status_code == 403 # Forbidden if no token
 
 def test_get_cpu_usage(client):
-    response = client.get('/cpu', headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwOTQwOTV9.pTMdQvQrm7GjvxzalwzFKBMjl1UKaBLckDb7A7jPkvc'})
+    response = client.get('/cpu', headers={'Authorization': auth_token(client)})
     assert response.status_code == 200
 
 def test_get_memory_usage(client):
-    response = client.get('/memory', headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwOTQwOTV9.pTMdQvQrm7GjvxzalwzFKBMjl1UKaBLckDb7A7jPkvc'})
+    response = client.get('/memory', headers={'Authorization': auth_token(client)})
     assert response.status_code == 200
 
 def test_get_disk_usage(client):
-    response = client.get('/disk', headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwOTQwOTV9.pTMdQvQrm7GjvxzalwzFKBMjl1UKaBLckDb7A7jPkvc'})
+    response = client.get('/disk', headers={'Authorization': auth_token(client)})
     assert response.status_code == 200
 
 def test_get_bandwidth_usage(client):
-    response = client.get('/bandwidth', headers={'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzIwOTQwOTV9.pTMdQvQrm7GjvxzalwzFKBMjl1UKaBLckDb7A7jPkvc'})
+    response = client.get('/bandwidth', headers={'Authorization': auth_token(client)})
     assert response.status_code == 200
 
 def test_get_token(client):
